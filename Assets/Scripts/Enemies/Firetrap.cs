b146bebac7bf77a49d9bf5434ddd7e15206d4c32
mariancosmin17 <161645867+mariancosmin17@users.noreply.github.com>
2026-03-23 20:17:56 +0200

    Adaugat capcana cu sageti

---
+++ Assets/Scripts/Enemies/Firetrap.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+public class Firetrap : MonoBehaviour
+{
+    [SerializeField] private float damage;
+    [Header("Firetrap Timers")]
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activeTime;
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+    
+    private bool triggered;//cand a fost triggered capcana
+    private bool active;//cand e activa si poate da damage
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag=="Player")
+        {
+            if(!triggered)
+            {
+                StartCoroutine(ActivateFiretrap());
+            }
+            if (active)
+                collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+
+    private IEnumerator ActivateFiretrap()
+    {
+        triggered = true;
+        spriteRend.color = Color.red;
+        yield return new WaitForSeconds(activationDelay);
+        spriteRend.color = Color.white;
+        active = true;
+        anim.SetBool("activated", true);
+        yield return new WaitForSeconds(activeTime);
+        active = false;
+        triggered = false;
+        anim.SetBool("activated", false);
+    }
+}
